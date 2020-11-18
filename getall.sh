12ac629da47ba046179c45c445e53620c9d7a6c4
KusaReMKN <pi@192.168.71.150>
2020-11-18 13:44:42 +0900

    add AutoIndex()
    URI の末尾が / で終わるときに index.html を勝手に付与する関数

--- getall.sh
+++ getall.sh
@@ -17,3 +17,10 @@ MIMETypeOf() {
   echo $(ContentTypeOf $1) \
     | sed -En -e 's/^([^;]*).*$/\1/p'
 }
+
+# / で終わるファイルには index.html を付与
+# AutoIndex([URI])
+AutoIndex() {
+  echo $1 \
+    | sed -E -e 's#(.*)/$#\1/index.html#'
+}
