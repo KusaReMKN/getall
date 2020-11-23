c6452fb25b09440bf0b7ddfe7f580e013b4b5253
KusaReMKN <kusaremkn@gmail.com>
2020-11-23 17:04:45 +0900

    fix getall.sh
    コマンド引数の変数部分に引用符を付加

--- getall.sh
+++ getall.sh
@@ -7,7 +7,7 @@ HEADER=HEAD
 # ContentTypeOf(URI)
 ContentTypeOf() {
   [ $# -eq 1 ] \
-    && $HEADER $1 \
+    && $HEADER "$1" \
     | sed -Ene 's/^[Cc]ontent-[Tt]ype:\s*(.*).*$/\1/p' \
     | tac
 }
@@ -15,14 +15,14 @@ ContentTypeOf() {
 # Content-Type の中の MIME を返す
 # MIMETypeOf(URI)
 MIMETypeOf() {
-  echo $(ContentTypeOf $1) \
+  echo "$(ContentTypeOf "$1")" \
     | sed -En -e 's/^([^;]*).*$/\1/p'
 }
 
 # / で終わるファイルには index.html を付与
 # AutoIndex([URI])
 AutoIndex() {
-  echo $1 \
+  echo "$1" \
     | sed -E -e 's#(.*)/$#\1/index.html#'
 }
 
@@ -32,9 +32,9 @@ BaseURI() {
   if [ -f "$1" ]; then
     base=$(sed -Ene 's/.*<base.*href="([^"]+)".*>.*/\1/pi' "$1" | tac)
     if [ -n "$base" ]; then
-      echo $base
+      echo "$base"
     else
-      echo $2
+      echo "$2"
     fi
   fi
 }
@@ -42,26 +42,26 @@ BaseURI() {
 # 完全修飾 URL からファイル名部分を取り去ったディレクトリを返す
 # DirURI([URI])
 DirURI() {
-  echo "$(echo $1 | sed -E -e 's#([^/])/[^/]*$#\1#')/"
+  echo "$(echo "$1" | sed -E -e 's#([^/])/[^/]*$#\1#')/"
 }
 
 # 完全修飾 URL からその親ディレクトリを指す URL を返す
 # ParentDirURI([URI])
 ParentDirURI() {
-  echo "$(DirURI $(DirURI $1 | sed -E -e 's#/$##'))"
+  echo "$(DirURI "$(DirURI "$1" | sed -E -e 's#/$##')")"
 }
 
 # 完全修飾 URL からその Root ディレクトリを指す URL を返す
 # RootDirURI([URI])
 RootDirURI() {
-  echo $1 \
+  echo "$1" \
     | sed -E -e 's#^(https?://[^/]*/).*$#\1#'
 }
 
 # 完全修飾 URL のスキームを返す
 # SchemeOf([URI])
 SchemeOf() {
-  echo $1 \
+  echo "$1" \
     | sed -E -e 's/^([^:]*):.*$/\1/'
 }
 
@@ -82,7 +82,7 @@ FullURI() {
 # HostAndDir(URI)
 HostAndDir() {
   [ -n "$1" ] \
-    && echo $(DirURI "$(echo "$1" | sed -E -e 's#.*://(.*)#\1#')")
+    && echo "$(DirURI "$(echo "$1" | sed -E -e 's#.*://(.*)#\1#')")"
 }
 
 # これは考え直したほうがいいかもしれない
@@ -90,18 +90,18 @@ HostAndDir() {
 # LinkList(URI)
 LinkList() {
   if [ $# -eq 1 ]; then
-    if [ "$(MIMETypeOf $1)" = "text/html" ]; then
+    if [ "$(MIMETypeOf "$1")" = "text/html" ]; then
       tmpfile=$(mktemp)
 
       echo "# $1" > $tmpfile
-      $GETTER $1 \
+      $GETTER "$1" \
         | sed -En -e 's/^.*href="([^"]+)".*$/\1/p' \
                   -e 's/^.*src="([^"]+)".*$/\1/p' \
         | sort \
         | uniq \
-        >> $tmpfile
+        >> "$tmpfile"
 
-      echo $tmpfile
+      echo "$tmpfile"
     fi
   fi
 }
